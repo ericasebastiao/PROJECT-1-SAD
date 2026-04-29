a4a5cf4cedb1c209ccef3995d9c81548ef600670
ericasebastiao <ericasebastiao2002@gmail.com>
2026-04-29 09:11:14 +0100

    Update digitalIO.c
    New digital_read() function

--- digitalIO.c
+++ digitalIO.c
@@ -2,17 +2,29 @@
  * File:   digitalIO.c
  * Author: Faculdade
  *
- * Created on 31 de Março de 2026, 12:33
+ * Created on 31 de MarÃ§o de 2026, 12:33
  */
 
 
 #include "xc.h"
 #include "digitalIO.h"
 
-int digital_Read()
+int digital_Read(int portNumber)
+{
+    switch(portNumber){
+        case 54: return (!PORTDbits.RD6);
+        
+        case 55: return (!PORTDbits.RD7);
+        
+        default: return -1;
+    }
+    
+}
+
+/*int digital_Read()
 {
     return (!PORTDbits.RD6);//Switch case needed
-}
+}*/
 
 void digital_Write(int n)
 {
